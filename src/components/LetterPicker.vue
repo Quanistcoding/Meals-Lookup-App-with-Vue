e7dbac5bcc148dc92927da147f10d0549e6749f3
unknown <jenchihhung@hotmail.com>
2023-04-21 11:07:05 +0800

    Refactored letter picker into a seperate component.

---
+++ src/components/LetterPicker.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class = "flex gap-2 justify-center my-2">
+            <button :key = "letter" v-for = "letter of letters.split('')" @click = "setCurrentLetter(letter)"
+                :class = "{'font-bold':currentLetter == letter,'text-red-500':currentLetter == letter}"
+            >
+                <span>{{letter}}</span>
+            </button>
+         </div>
+    </div>
+</template>
+<script setup>
+import { computed } from '@vue/reactivity';
+import store from '../store';
+
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const currentLetter = computed(()=>store.state.searchLetter);
+function setCurrentLetter(letter){
+    store.dispatch('setSearchLetter',letter)
+}
+</script>
--- src/views/SearchByLetter.vue
+++ src/views/SearchByLetter.vue
@@ -1,26 +1,17 @@
 <template>
-    <div>
-        <div class = "flex gap-2 justify-center my-2">
-            <button :key = "letter" v-for = "letter of letters.split('')" @click = "setCurrentLetter(letter)"
-                :class = "{'font-bold':currentLetter == letter,'text-red-500':currentLetter == letter}"
-            >
-                <span>{{letter}}</span>
-            </button>
-         </div>
-    </div>
+    <LetterPicker />
     <meals :meals="meals"/>
 </template>
 <script setup>
 import { computed } from '@vue/reactivity';
 import store from '../store';
 import Meals from '../components/Meals.vue'
+import LetterPicker from '../components/LetterPicker.vue'
 
 
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const currentLetter = computed(()=>store.state.searchLetter);
 const meals = computed(()=>store.state.mealsByLetter);
 
-function setCurrentLetter(letter){
-    store.dispatch('setSearchLetter',letter)
-}
+
 </script>
